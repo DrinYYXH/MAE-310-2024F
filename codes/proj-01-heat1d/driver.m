d599a987fb2a902e8e358bf298a71f3085813aa8
Drin <Drin@DESKTOP-B656AC3>
2024-11-20 11:09:51 +0800

    achieve nodally exct!! in the method of divide compute into element

--- codes/proj-01-heat1d/driver.m
+++ codes/proj-01-heat1d/driver.m
@@ -114,6 +114,52 @@ disp = [d_temp; g];
 % 
 % plot(X,uu);
 
+%%
+
+% maybe a stupid way that make a large array just call it IP array, about the same as IEN array
+% IP array is mainly the find every point of uu from partial coor to total coor
+
+tt = 5;                                 %divide the points in each partial element
+ipn = 1 : 1/tt : n_en;                  %make each points a mark
+
+dd = (tt - 1)*(n_en - 1) + n_en;        %in order to make IP easy
+IP = zeros(n_en,length(ipn));
+for ee = 1 : n_el
+    for xip = 1 : length(ipn)
+        IP(ee,xip) = xip + (dd - 1)*(ee - 1);
+    end
+end
+
+
+uu = zeros(IP(n_el,length(ipn)),1);     %u_h actually
+x_xii = zeros(IP(n_el,length(ipn)),1);  %the x of each u_h in total coor
+
+for ee = 1 : n_el
+    for xip = 1 : length(ipn)
+        for aa = 1 : n_en
+        xii = 2*(xip - 1)/(length(ipn) - 1) - 1;
+
+        x_xii(IP(ee,xip)) = x_xii(IP(ee,xip)) + x_coor(IEN(ee,aa)) * PolyShape(pp,aa,xii,0);
+
+        uu(IP(ee,xip)) = uu(IP(ee,xip)) + disp(IEN(ee,aa)) * PolyShape(pp,aa,xii,0);
+
+        end
+
+        if (ee > 1) && (xip == 1)
+            x_xii(IP(ee,xip)) = x_xii(IP(ee,xip))/2;
+            uu(IP(ee,xip)) = uu(IP(ee,xip))/2;
+        end
+
+    end
+end
+
+% draw a picture of u_h-x compare with u-x
+figure(1)
+plot(x_xii,uu,'xr');
+hold on;
+plot(x_xii,x_xii.^5,'k');
+
+
 
 
 
